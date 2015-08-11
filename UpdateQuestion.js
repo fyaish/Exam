925181994e9f14952ad2570fe72669da008f4296
fyaish <fuad_yaish@hotmail.com>
2015-08-11 18:00:27 +0300

    Update Question Sample
    q1 q2 updatequestion pages

---
+++ UpdateQuestion.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("sampleApp", ["firebase"]);
+
+app.controller("SampleCtrl", function ($scope, $firebaseObject, $firebaseArray) {
+    var refQuestion = new Firebase("https://amber-torch-2469.firebaseio.com/UpdateQuestioID");
+
+    // download the data into a local object
+    var syncObjectQuestion = $firebaseObject(refQuestion);
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObjectQuestion.$bindTo($scope, "q");
+
+    //////////////
+
+    var refMessages = refQuestion.child('messages');
+
+    // create a synchronized array
+    $scope.messages = $firebaseArray(refMessages);
+
+    // add new items to the array
+    // the message is automatically added to our Firebase database!
+    $scope.addMessage = function () {
+        $scope.messages.$add({
+            text: $scope.newMessageText
+        });
+        $scope.newMessageText = "";
+    };
+
+});
---
+++ q2.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("sampleApp", ["firebase"]);
+
+app.controller("SampleCtrl", function ($scope, $firebaseArray) {
+    var ref = new Firebase("https://amber-torch-2469.firebaseio.com/messages");
+
+    // create a synchronized array
+    $scope.messages = $firebaseArray(ref);
+
+    // add new items to the array
+    // the message is automatically added to our Firebase database!
+    $scope.addMessage = function () {
+        $scope.messages.$add({
+            text: $scope.newMessageText
+        });
+        $scope.newMessageText = ""; 
+    };
+
+    // click on `index.html` above to see $remove() and $save() in action
+});
